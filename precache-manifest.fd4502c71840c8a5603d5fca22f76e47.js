bdd5c99954fda306f6971636903d1e6081d60bac
Moriarty-jim <47500237+Moriarty-jim@users.noreply.github.com>
2020-07-13 16:59:28 +0545

    Updates

---
+++ precache-manifest.fd4502c71840c8a5603d5fca22f76e47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f3595efd2502b0f503c20e30484255",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "1f935087ec9ad1cfe994",
+    "url": "/my-cv/static/css/main.d4daaa16.chunk.css"
+  },
+  {
+    "revision": "15f9ec28543bab79068b",
+    "url": "/my-cv/static/js/2.82d60775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv/static/js/2.82d60775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f935087ec9ad1cfe994",
+    "url": "/my-cv/static/js/main.8e746f1c.chunk.js"
+  },
+  {
+    "revision": "b7f57c7aa00c919803af",
+    "url": "/my-cv/static/js/runtime-main.09c69248.js"
+  },
+  {
+    "revision": "4c4bd2129872268b15bcc5e550e4e02d",
+    "url": "/my-cv/static/media/me.4c4bd212.jpg"
+  }
+]);
